d053c8c1807802ae9691037d817e70f74f3615aa
Marek Kubica <marek@xivilization.net>
2018-10-08 15:42:07 +0200

    Also update the tests to dune

--- test/test.ml
+++ test/test.ml
@@ -2,9 +2,6 @@ let suite = [
   ("path relative to source file", `Quick, fun () ->
     Alcotest.(check string) "file contents" "foo\n" [%blob "test_file"]
   );
-  ("path relative to working directory", `Quick, fun () ->
-    Alcotest.(check string) "file contents" "foo\n" [%blob "../../test/test_file"]
-  )
 ]
 
 let () =
