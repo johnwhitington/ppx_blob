9d83f441f3c709998a1454468e048382d3aa2e92
Dave Aitken <dave.aitken@gmail.com>
2022-07-12 16:34:28 +0100

    add tests for blobs from relative parent and sub dir

--- test/test.ml
+++ test/test.ml
@@ -2,6 +2,12 @@ let suite = [
   ("path relative to source file", `Quick, fun () ->
     Alcotest.(check string) "file contents" "foo\n" [%blob "test_file"]
   );
+  ("path relative to source file (subdir)", `Quick, fun () ->
+    Alcotest.(check string) "file contents" "subdir foo\n" [%blob "subdir/test_file"]
+  );
+  ("path relative to source file (parent dir)", `Quick, fun () ->
+    Alcotest.(check string) "file contents" "parent foo\n" [%blob "../test_file"]
+  );
 ]
 
 let () =
